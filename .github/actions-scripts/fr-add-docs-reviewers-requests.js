2ee6eb65e1646ca42494da61ea201d9ad7faa09e
Sarah Edwards <skedwards88@github.com>
2021-07-16 14:01:44 -0700

    Update fr-add-docs-reviewers-requests.js

--- .github/actions-scripts/fr-add-docs-reviewers-requests.js
+++ .github/actions-scripts/fr-add-docs-reviewers-requests.js
@@ -4,9 +4,10 @@ const { graphql } = require("@octokit/graphql");
 // adds the PRs/issues to the project
 // and returns the node IDs of the project items
 async function addItemsToProject(items, project) {
-  console.log(`Adding ${items} to project ${project}`)
+  console.log(`Adding ${items} to project ${project}`);
 
-  const mutations = items.map((pr, index) => `
+  const mutations = items.map(
+    (pr, index) => `
     pr_${index}: addProjectNextItem(input: {
       projectId: $project
       contentId: "${pr}"
@@ -16,30 +17,29 @@ async function addItemsToProject(items, project) {
       }
     }
     `
-  )
+  );
 
   const mutation = `
   mutation($project:ID!) {
-    ${mutations.join(' ')}
+    ${mutations.join(" ")}
   }
-  `
+  `;
 
-  const newItems = await graphql(
-    mutation,
-    {
-      project: project,
-      headers: {
-        authorization: `token ${process.env.TOKEN}`,
-        "GraphQL-Features": "projects_next_graphql",
-      },
-    }
+  const newItems = await graphql(mutation, {
+    project: project,
+    headers: {
+      authorization: `token ${process.env.TOKEN}`,
+      "GraphQL-Features": "projects_next_graphql",
+    },
+  });
+
+  const newItemIDs = Object.entries(newItems).map(
+    (item) => item[1].projectNextItem.id
   );
 
-  const newItemIDs = Object.entries(newItems).map(item => item[1].projectNextItem.id)
+  console.log(`New item IDs: ${newItemIDs}`);
 
-  console.log(`New item IDs: ${newItemIDs}`)
-
-  return newItemIDs
+  return newItemIDs;
 }
 
 // Given a list of project item node IDs and a list of corresponding authors
@@ -51,38 +51,41 @@ async function addItemsToProject(items, project) {
 //   - "Contributor type"" (as "Hubber or partner" option)
 // Does not populate "Review needs" or "Size"
 function generateUpdateProjectNextItemFieldMutation(items, authors) {
-
   // Formats a date object into the required format for projects
   function formatDate(date) {
-    return date.getFullYear() + '-' + date.getMonth() + '-' + date.getDate()
+    return date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate();
   }
 
   // Calculate 2 weekdays from now (excluding weekends; not considering holidays)
-  const datePosted = new Date()
-  let daysUntilDue
+  const datePosted = new Date();
+  let daysUntilDue;
   switch (datePosted.getDay()) {
     case 0: // Sunday
-      daysUntilDue = 3
+      daysUntilDue = 3;
       break;
     case 6: // Saturday
-      daysUntilDue = 4
+      daysUntilDue = 4;
       break;
     default:
-      daysUntilDue = 2
+      daysUntilDue = 2;
   }
-  const dueDate = new Date(datePosted.getTime() + (24 * 60 * 60 * 1000 * daysUntilDue))
+  const dueDate = new Date(
+    datePosted.getTime() + 24 * 60 * 60 * 1000 * daysUntilDue
+  );
 
   // Build the mutation for a single field
   function generateMutation(index, item, fieldID, value, literal = false) {
-    let parsedValue
+    let parsedValue;
     if (literal) {
-      parsedValue = `value: "${value}"`
+      parsedValue = `value: "${value}"`;
     } else {
-      parsedValue = `value: ${value}`
+      parsedValue = `value: ${value}`;
     }
 
     return `
-      set_${fieldID.substr(1)}_item_${index}: updateProjectNextItemField(input: {
+      set_${fieldID.substr(
+        1
+      )}_item_${index}: updateProjectNextItemField(input: {
         projectId: $project
         itemId: "${item}"
         fieldId: ${fieldID}
@@ -92,19 +95,38 @@ function generateUpdateProjectNextItemFieldMutation(items, authors) {
         id
       }
     }
-    `
+    `;
   }
 
   // Build the mutation for all fields for all items
-  const mutations = items.map((item, index) => `
+  const mutations = items.map(
+    (item, index) => `
     ${generateMutation(index, item, "$statusID", "$readyForReviewID")}
-    ${generateMutation(index, item, "$datePostedID", formatDate(datePosted), true)}
-    ${generateMutation(index, item, "$reviewDueDateID", formatDate(dueDate), true)}
+    ${generateMutation(
+      index,
+      item,
+      "$datePostedID",
+      formatDate(datePosted),
+      true
+    )}
+    ${generateMutation(
+      index,
+      item,
+      "$reviewDueDateID",
+      formatDate(dueDate),
+      true
+    )}
     ${generateMutation(index, item, "$contributorTypeID", "$hubberTypeID")}
-    ${generateMutation(index, item, "$featureID", "OpenAPI schema update", true)}
+    ${generateMutation(
+      index,
+      item,
+      "$featureID",
+      "OpenAPI schema update",
+      true
+    )}
     ${generateMutation(index, item, "$authorID", authors[index], true)}
   `
-  )
+  );
 
   // Build the full mutation
   const mutation = `
@@ -120,15 +142,14 @@ function generateUpdateProjectNextItemFieldMutation(items, authors) {
       $authorID: ID!
 
     ) {
-      ${mutations.join(' ')}
+      ${mutations.join(" ")}
     }
-    `
+    `;
 
-  return mutation
+  return mutation;
 }
 
 async function run() {
-
   // Get info about the docs-content review board project
   // and about open github/github PRs
   const data = await graphql(
@@ -147,7 +168,7 @@ async function run() {
                 id
               }
             }
-            fields(first:20) {
+            fields(first: 20) {
               nodes {
                 id
                 name
@@ -155,8 +176,8 @@ async function run() {
               }
             }
           }
-        },
-        repository(name: $repo, owner: $organization){
+        }
+        repository(name: $repo, owner: $organization) {
           pullRequests(last: $num_prs, states: OPEN) {
             nodes {
               id
@@ -175,16 +196,16 @@ async function run() {
                   name
                 }
               }
-              reviews(first: 10){
-                nodes{
-                  onBehalfOf(first: 1){
-                    nodes{
+              reviews(first: 10) {
+                nodes {
+                  onBehalfOf(first: 1) {
+                    nodes {
                       name
                     }
                   }
                 }
               }
-              author{
+              author {
                 login
               }
             }
@@ -209,86 +230,112 @@ async function run() {
   // - not a train
   // - are requesting a review by docs-reviewers
   // - have not already been reviewed on behalf of docs-reviewers
-  const prs = data.repository.pullRequests.nodes.filter(pr =>
-    !pr.isDraft
-    && !pr.labels.nodes.find(label => label.name === "Deploy train 🚂")
-    && pr.reviewRequests.nodes.find(requestedReviewers => requestedReviewers.requestedReviewer.name === process.env.REVIEWER)
-    && !pr.reviews.nodes.flatMap(review => review.onBehalfOf.nodes).find(behalf => behalf.name === process.env.REVIEWER)
-  )
+  const prs = data.repository.pullRequests.nodes.filter(
+    (pr) =>
+      !pr.isDraft &&
+      !pr.labels.nodes.find((label) => label.name === "Deploy train 🚂") &&
+      pr.reviewRequests.nodes.find(
+        (requestedReviewers) =>
+          requestedReviewers.requestedReviewer.name === process.env.REVIEWER
+      ) &&
+      !pr.reviews.nodes
+        .flatMap((review) => review.onBehalfOf.nodes)
+        .find((behalf) => behalf.name === process.env.REVIEWER)
+  );
   if (prs.length === 0) {
-    console.log("No PRs found. Exiting.")
-    return
+    console.log("No PRs found. Exiting.");
+    return;
   }
 
-  const prIDs = prs.map(pr => pr.id)
-  const prAuthors = prs.map(pr => pr.author.login)
-  console.log(`PRs found: ${prIDs}`)
+  const prIDs = prs.map((pr) => pr.id);
+  const prAuthors = prs.map((pr) => pr.author.login);
+  console.log(`PRs found: ${prIDs}`);
 
   // Get the project ID
-  const projectID = data.organization.projectNext.id
+  const projectID = data.organization.projectNext.id;
 
   // Get the IDs of the last 100 items on the board
   // Until we have a way to check from a PR whether the PR is in a project,
   // this is how we (roughly) avoid overwriting PRs that are already on the board
   // If we are overwriting items, query for more items
-  const existingItemIDs = data.organization.projectNext.items.nodes.map(node => node.id)
+  const existingItemIDs = data.organization.projectNext.items.nodes.map(
+    (node) => node.id
+  );
 
   // Get the ID of the fields that we want to populate
-  const datePostedID = data.organization.projectNext.fields.nodes.find(field => field.name === "Date posted").id
-  const reviewDueDateID = data.organization.projectNext.fields.nodes.find(field => field.name === "Review due date").id
-  const statusID = data.organization.projectNext.fields.nodes.find(field => field.name === "Status").id
-  const featureID = data.organization.projectNext.fields.nodes.find(field => field.name === "Feature").id
-  const contributorTypeID = data.organization.projectNext.fields.nodes.find(field => field.name === "Contributor type").id
-  const authorID = data.organization.projectNext.fields.nodes.find(field => field.name === "Author").id
+  const datePostedID = data.organization.projectNext.fields.nodes.find(
+    (field) => field.name === "Date posted"
+  ).id;
+  const reviewDueDateID = data.organization.projectNext.fields.nodes.find(
+    (field) => field.name === "Review due date"
+  ).id;
+  const statusID = data.organization.projectNext.fields.nodes.find(
+    (field) => field.name === "Status"
+  ).id;
+  const featureID = data.organization.projectNext.fields.nodes.find(
+    (field) => field.name === "Feature"
+  ).id;
+  const contributorTypeID = data.organization.projectNext.fields.nodes.find(
+    (field) => field.name === "Contributor type"
+  ).id;
+  const authorID = data.organization.projectNext.fields.nodes.find(
+    (field) => field.name === "Author"
+  ).id;
 
   // Get the ID of the single select values that we want to set
-  const readyForReviewID = JSON.parse(data.organization.projectNext.fields.nodes.find(field => field.name === "Status").settings).options.find(field => field.name === "Ready for review").id
-  const hubberTypeID = JSON.parse(data.organization.projectNext.fields.nodes.find(field => field.name === "Contributor type").settings).options.find(field => field.name === "Hubber or partner").id
+  const readyForReviewID = JSON.parse(
+    data.organization.projectNext.fields.nodes.find(
+      (field) => field.name === "Status"
+    ).settings
+  ).options.find((field) => field.name === "Ready for review").id;
+  const hubberTypeID = JSON.parse(
+    data.organization.projectNext.fields.nodes.find(
+      (field) => field.name === "Contributor type"
+    ).settings
+  ).options.find((field) => field.name === "Hubber or partner").id;
 
   // Add the PRs to the project
-  const itemIDs = await addItemsToProject(prIDs, projectID)
+  const itemIDs = await addItemsToProject(prIDs, projectID);
 
   // If an item already existed on the project, the existing ID will be returned.
   // Exclude existing items going forward.
   // Until we have a way to check from a PR whether the PR is in a project,
   // this is how we (roughly) avoid overwriting PRs that are already on the board
-  const newItemIDs = itemIDs.filter(id => !existingItemIDs.includes(id) )
+  const newItemIDs = itemIDs.filter((id) => !existingItemIDs.includes(id));
 
   if (newItemIDs.length === 0) {
-    console.log("All found PRs are already on the project. Exiting.")
-    return
+    console.log("All found PRs are already on the project. Exiting.");
+    return;
   }
 
   // Populate fields for the new project items
-  // Note: Since there is not a way to check if a PR is already on the board, 
+  // Note: Since there is not a way to check if a PR is already on the board,
   // this will overwrite the values of PRs that are on the board
-  const updateProjectNextItemMutation = generateUpdateProjectNextItemFieldMutation(newItemIDs, prAuthors)
-  console.log(`Populating fields for these items: ${newItemIDs}`)
+  const updateProjectNextItemMutation =
+    generateUpdateProjectNextItemFieldMutation(newItemIDs, prAuthors);
+  console.log(`Populating fields for these items: ${newItemIDs}`);
 
-  await graphql(
-    updateProjectNextItemMutation,
-    {
-      project: projectID,
-      statusID: statusID,
-      readyForReviewID: readyForReviewID,
-      datePostedID: datePostedID,
-      reviewDueDateID: reviewDueDateID,
-      contributorTypeID: contributorTypeID,
-      hubberTypeID: hubberTypeID,
-      featureID: featureID,
-      authorID: authorID,
-      headers: {
-        authorization: `token ${process.env.TOKEN}`,
-        "GraphQL-Features": "projects_next_graphql",
-      },
-    }
-  );
-  console.log('Done populating fields')
+  await graphql(updateProjectNextItemMutation, {
+    project: projectID,
+    statusID: statusID,
+    readyForReviewID: readyForReviewID,
+    datePostedID: datePostedID,
+    reviewDueDateID: reviewDueDateID,
+    contributorTypeID: contributorTypeID,
+    hubberTypeID: hubberTypeID,
+    featureID: featureID,
+    authorID: authorID,
+    headers: {
+      authorization: `token ${process.env.TOKEN}`,
+      "GraphQL-Features": "projects_next_graphql",
+    },
+  });
+  console.log("Done populating fields");
 
-  return newItemIDs
+  return newItemIDs;
 }
 
 run().catch((error) => {
-  console.log(`#ERROR# ${error}`)
-  process.exit(1)
+  console.log(`#ERROR# ${error}`);
+  process.exit(1);
 });
