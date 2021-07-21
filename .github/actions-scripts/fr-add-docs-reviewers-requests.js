d2278e5aee7e45d83c619cd935f7de04aaf4cd76
Sarah Edwards <skedwards88@github.com>
2021-07-21 10:47:44 -0700

    Update fr-add-docs-reviewers-requests.js

--- .github/actions-scripts/fr-add-docs-reviewers-requests.js
+++ .github/actions-scripts/fr-add-docs-reviewers-requests.js
@@ -1,10 +1,10 @@
-const { graphql } = require("@octokit/graphql");
+const { graphql } = require('@octokit/graphql')
 
 // Given a list of PR/issue node IDs and a project node ID,
 // adds the PRs/issues to the project
 // and returns the node IDs of the project items
 async function addItemsToProject(items, project) {
-  console.log(`Adding ${items} to project ${project}`);
+  console.log(`Adding ${items} to project ${project}`)
 
   const mutations = items.map(
     (pr, index) => `
@@ -17,29 +17,27 @@ async function addItemsToProject(items, project) {
       }
     }
     `
-  );
+  )
 
   const mutation = `
   mutation($project:ID!) {
-    ${mutations.join(" ")}
+    ${mutations.join(' ')}
   }
-  `;
+  `
 
   const newItems = await graphql(mutation, {
     project: project,
     headers: {
       authorization: `token ${process.env.TOKEN}`,
-      "GraphQL-Features": "projects_next_graphql",
+      'GraphQL-Features': 'projects_next_graphql',
     },
-  });
+  })
 
-  const newItemIDs = Object.entries(newItems).map(
-    (item) => item[1].projectNextItem.id
-  );
+  const newItemIDs = Object.entries(newItems).map((item) => item[1].projectNextItem.id)
 
-  console.log(`New item IDs: ${newItemIDs}`);
+  console.log(`New item IDs: ${newItemIDs}`)
 
-  return newItemIDs;
+  return newItemIDs
 }
 
 // Given a list of project item node IDs and a list of corresponding authors
@@ -53,39 +51,35 @@ async function addItemsToProject(items, project) {
 function generateUpdateProjectNextItemFieldMutation(items, authors) {
   // Formats a date object into the required format for projects
   function formatDate(date) {
-    return date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate();
+    return date.getFullYear() + '-' + date.getMonth() + '-' + date.getDate()
   }
 
   // Calculate 2 weekdays from now (excluding weekends; not considering holidays)
-  const datePosted = new Date();
-  let daysUntilDue;
+  const datePosted = new Date()
+  let daysUntilDue
   switch (datePosted.getDay()) {
     case 0: // Sunday
-      daysUntilDue = 3;
-      break;
+      daysUntilDue = 3
+      break
     case 6: // Saturday
-      daysUntilDue = 4;
-      break;
+      daysUntilDue = 4
+      break
     default:
-      daysUntilDue = 2;
+      daysUntilDue = 2
   }
-  const dueDate = new Date(
-    datePosted.getTime() + 24 * 60 * 60 * 1000 * daysUntilDue
-  );
+  const dueDate = new Date(datePosted.getTime() + 24 * 60 * 60 * 1000 * daysUntilDue)
 
   // Build the mutation for a single field
   function generateMutation(index, item, fieldID, value, literal = false) {
-    let parsedValue;
+    let parsedValue
     if (literal) {
-      parsedValue = `value: "${value}"`;
+      parsedValue = `value: "${value}"`
     } else {
-      parsedValue = `value: ${value}`;
+      parsedValue = `value: ${value}`
     }
 
     return `
-      set_${fieldID.substr(
-        1
-      )}_item_${index}: updateProjectNextItemField(input: {
+      set_${fieldID.substr(1)}_item_${index}: updateProjectNextItemField(input: {
         projectId: $project
         itemId: "${item}"
         fieldId: ${fieldID}
@@ -95,38 +89,20 @@ function generateUpdateProjectNextItemFieldMutation(items, authors) {
         id
       }
     }
-    `;
+    `
   }
 
   // Build the mutation for all fields for all items
   const mutations = items.map(
     (item, index) => `
-    ${generateMutation(index, item, "$statusID", "$readyForReviewID")}
-    ${generateMutation(
-      index,
-      item,
-      "$datePostedID",
-      formatDate(datePosted),
-      true
-    )}
-    ${generateMutation(
-      index,
-      item,
-      "$reviewDueDateID",
-      formatDate(dueDate),
-      true
-    )}
-    ${generateMutation(index, item, "$contributorTypeID", "$hubberTypeID")}
-    ${generateMutation(
-      index,
-      item,
-      "$featureID",
-      "OpenAPI schema update",
-      true
-    )}
-    ${generateMutation(index, item, "$authorID", authors[index], true)}
+    ${generateMutation(index, item, '$statusID', '$readyForReviewID')}
+    ${generateMutation(index, item, '$datePostedID', formatDate(datePosted), true)}
+    ${generateMutation(index, item, '$reviewDueDateID', formatDate(dueDate), true)}
+    ${generateMutation(index, item, '$contributorTypeID', '$hubberTypeID')}
+    ${generateMutation(index, item, '$featureID', 'OpenAPI schema update', true)}
+    ${generateMutation(index, item, '$authorID', authors[index], true)}
   `
-  );
+  )
 
   // Build the full mutation
   const mutation = `
@@ -142,11 +118,11 @@ function generateUpdateProjectNextItemFieldMutation(items, authors) {
       $authorID: ID!
 
     ) {
-      ${mutations.join(" ")}
+      ${mutations.join(' ')}
     }
-    `;
+    `
 
-  return mutation;
+  return mutation
 }
 
 async function run() {
@@ -154,12 +130,7 @@ async function run() {
   // and about open github/github PRs
   const data = await graphql(
     `
-      query (
-        $organization: String!
-        $repo: String!
-        $projectNumber: Int!
-        $num_prs: Int!
-      ) {
+      query ($organization: String!, $repo: String!, $projectNumber: Int!, $num_prs: Int!) {
         organization(login: $organization) {
           projectNext(number: $projectNumber) {
             id
@@ -220,10 +191,10 @@ async function run() {
       num_prs: parseInt(process.env.NUM_PRS),
       headers: {
         authorization: `token ${process.env.TOKEN}`,
-        "GraphQL-Features": "projects_next_graphql",
+        'GraphQL-Features': 'projects_next_graphql',
       },
     }
-  );
+  )
 
   // Get the PRs that are:
   // - not draft
@@ -233,96 +204,97 @@ async function run() {
   const prs = data.repository.pullRequests.nodes.filter(
     (pr) =>
       !pr.isDraft &&
-      !pr.labels.nodes.find((label) => label.name === "Deploy train 🚂") &&
+      !pr.labels.nodes.find((label) => label.name === 'Deploy train 🚂') &&
       pr.reviewRequests.nodes.find(
-        (requestedReviewers) =>
-          requestedReviewers.requestedReviewer.name === process.env.REVIEWER
+        (requestedReviewers) => requestedReviewers.requestedReviewer.name === process.env.REVIEWER
       ) &&
       !pr.reviews.nodes
         .flatMap((review) => review.onBehalfOf.nodes)
         .find((behalf) => behalf.name === process.env.REVIEWER)
-  );
+  )
   if (prs.length === 0) {
-    console.log("No PRs found. Exiting.");
-    return;
+    console.log('No PRs found. Exiting.')
+    return
   }
 
-  const prIDs = prs.map((pr) => pr.id);
-  const prAuthors = prs.map((pr) => pr.author.login);
-  console.log(`PRs found: ${prIDs}`);
+  const prIDs = prs.map((pr) => pr.id)
+  const prAuthors = prs.map((pr) => pr.author.login)
+  console.log(`PRs found: ${prIDs}`)
 
   // Get the project ID
-  const projectID = data.organization.projectNext.id;
+  const projectID = data.organization.projectNext.id
 
   // Get the IDs of the last 100 items on the board.
   // Until we have a way to check from a PR whether the PR is in a project,
   // this is how we (roughly) avoid overwriting PRs that are already on the board.
   // If we are overwriting items, query for more items.
-  const existingItemIDs = data.organization.projectNext.items.nodes.map(
-    (node) => node.id
-  );
+  const existingItemIDs = data.organization.projectNext.items.nodes.map((node) => node.id)
 
   function findFieldID(fieldName, data) {
-    const field =  data.organization.projectNext.fields.nodes.find(
+    const field = data.organization.projectNext.fields.nodes.find(
       (field) => field.name === fieldName
     )
 
     if (field && field.id) {
       return field.id
     } else {
-      throw(`A field called ${fieldName} was not found. Check if the field was renamed.`)
+      throw new Error(`A field called ${fieldName} was not found. Check if the field was renamed.`)
     }
   }
 
   function findSingleSelectID(singleSelectName, fieldName, data) {
-    const field =  data.organization.projectNext.fields.nodes.find(
+    const field = data.organization.projectNext.fields.nodes.find(
       (field) => field.name === fieldName
     )
     if (!field) {
-      throw(`A field called ${fieldName} was not found. Check if the field was renamed.`)
+      throw new Error(`A field called ${fieldName} was not found. Check if the field was renamed.`)
     }
 
-    const singleSelect = JSON.parse(field.settings).options.find((field) => field.name === singleSelectName)
+    const singleSelect = JSON.parse(field.settings).options.find(
+      (field) => field.name === singleSelectName
+    )
 
     if (singleSelect && singleSelect.id) {
       return singleSelect.id
     } else {
-      throw(`A single select called ${singleSelectName} for the field ${fieldName} was not found. Check if the single select was renamed.`)
+      throw new Error(`A single select called ${singleSelectName} for the field ${fieldName} was not found. Check if the single select was renamed.`)
     }
   }
 
   // Get the ID of the fields that we want to populate
-  const datePostedID = findFieldID("Date posted", data)
-  const reviewDueDateID = findFieldID("Review due date", data)
-  const statusID = findFieldID("Status", data)
-  const featureID = findFieldID("Feature", data)
-  const contributorTypeID = findFieldID("Contributor type", data)
-  const authorID = findFieldID("Author", data)
+  const datePostedID = findFieldID('Date posted', data)
+  const reviewDueDateID = findFieldID('Review due date', data)
+  const statusID = findFieldID('Status', data)
+  const featureID = findFieldID('Feature', data)
+  const contributorTypeID = findFieldID('Contributor type', data)
+  const authorID = findFieldID('Author', data)
 
   // Get the ID of the single select values that we want to set
-  const readyForReviewID = findSingleSelectID("Ready for review", "Status", data)
-  const hubberTypeID = findSingleSelectID("Hubber or partner", "Contributor type", data)
+  const readyForReviewID = findSingleSelectID('Ready for review', 'Status', data)
+  const hubberTypeID = findSingleSelectID('Hubber or partner', 'Contributor type', data)
 
   // Add the PRs to the project
-  const itemIDs = await addItemsToProject(prIDs, projectID);
+  const itemIDs = await addItemsToProject(prIDs, projectID)
 
   // If an item already existed on the project, the existing ID will be returned.
   // Exclude existing items going forward.
   // Until we have a way to check from a PR whether the PR is in a project,
   // this is how we (roughly) avoid overwriting PRs that are already on the board
-  const newItemIDs = itemIDs.filter((id) => !existingItemIDs.includes(id));
+  const newItemIDs = itemIDs.filter((id) => !existingItemIDs.includes(id))
 
   if (newItemIDs.length === 0) {
-    console.log("All found PRs are already on the project. Exiting.");
-    return;
+    console.log('All found PRs are already on the project. Exiting.')
+    return
   }
 
   // Populate fields for the new project items
   // Note: Since there is not a way to check if a PR is already on the board,
   // this will overwrite the values of PRs that are on the board
-  const updateProjectNextItemMutation =
-    generateUpdateProjectNextItemFieldMutation(newItemIDs, prAuthors);
-  console.log(`Populating fields for these items: ${newItemIDs}`);
+  const updateProjectNextItemMutation = generateUpdateProjectNextItemFieldMutation(
+    newItemIDs,
+    prAuthors
+  )
+  console.log(`Populating fields for these items: ${newItemIDs}`)
 
   await graphql(updateProjectNextItemMutation, {
     project: projectID,
@@ -336,15 +308,15 @@ async function run() {
     authorID: authorID,
     headers: {
       authorization: `token ${process.env.TOKEN}`,
-      "GraphQL-Features": "projects_next_graphql",
+      'GraphQL-Features': 'projects_next_graphql',
     },
-  });
-  console.log("Done populating fields");
+  })
+  console.log('Done populating fields')
 
-  return newItemIDs;
+  return newItemIDs
 }
 
 run().catch((error) => {
-  console.log(`#ERROR# ${error}`);
-  process.exit(1);
-});
+  console.log(`#ERROR# ${error}`)
+  process.exit(1)
+})
