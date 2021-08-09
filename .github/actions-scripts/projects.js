3e637992ea414bde755dcdb4a973eac677b6fa4c
Sarah Edwards <skedwards88@github.com>
2021-08-09 14:22:47 -0700

    Update projects.js

--- .github/actions-scripts/projects.js
+++ .github/actions-scripts/projects.js
@@ -1,5 +1,44 @@
 import { graphql } from '@octokit/graphql'
 
+// Pull out the node ID of a field
+export function findFieldID(fieldName, data) {
+  const field = data.organization.projectNext.fields.nodes.find(
+    (field) => field.name === fieldName
+  )
+
+  if (field && field.id) {
+    return field.id
+  } else {
+    throw new Error(
+      `A field called "${fieldName}" was not found. Check if the field was renamed.`
+    )
+  }
+}
+
+// Pull out the node ID of a single select field value
+export function findSingleSelectID(singleSelectName, fieldName, data) {
+  const field = data.organization.projectNext.fields.nodes.find(
+    (field) => field.name === fieldName
+  )
+  if (!field) {
+    throw new Error(
+      `A field called "${fieldName}" was not found. Check if the field was renamed.`
+    )
+  }
+
+  const singleSelect = JSON.parse(field.settings).options.find(
+    (field) => field.name === singleSelectName
+  )
+
+  if (singleSelect && singleSelect.id) {
+    return singleSelect.id
+  } else {
+    throw new Error(
+      `A single select called "${singleSelectName}" for the field "${fieldName}" was not found. Check if the single select was renamed.`
+    )
+  }
+}
+
 // Given a list of PR/issue node IDs and a project node ID,
 // adds the PRs/issues to the project
 // and returns the node IDs of the project items
@@ -43,4 +82,57 @@ export async function addItemsToProject(items, project) {
   return newItemIDs
 }
 
-export default { addItemsToProject }
+// Given a GitHub login, returns a bool indicating 
+// whether the login is part of the docs team
+async function docsTeamMemberQ(login) {
+  // Get all members of the docs team
+  const data = await graphql(
+    `
+      query {
+        organization(login: "github") {
+          team(slug: "docs") {
+            members {
+              nodes {
+                login
+              }
+            }
+          }
+        }
+      }
+    `,
+    {
+      headers: {
+        authorization: `token ${process.env.TOKEN}`,
+      },
+    }
+  )
+
+  const teamMembers = data.organization.team.members.nodes.map((entry) => entry.login)
+
+  return teamMembers.includes(login)
+}
+
+  // Formats a date object into the required format for projects
+  function formatDateForProject(date) {
+    return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate()
+  }
+
+  // Calculate the date {turnaround} business days from now (excluding weekends; not considering holidays)
+function calculateDueDate(datePosted, turnaround = 2) {
+  let daysUntilDue
+  switch (datePosted.getDay()) {
+    case 0: // Sunday
+      daysUntilDue = turnaround + 1
+      break
+    case 6: // Saturday
+      daysUntilDue = turnaround + 2
+      break
+    default:
+      daysUntilDue = turnaround
+  }
+  const millisecPerDay = 24 * 60 * 60 * 1000
+  const dueDate = new Date(datePosted.getTime() + millisecPerDay * daysUntilDue)
+  return dueDate
+}
+
+export default { addItemsToProject, docsTeamMemberQ, findFieldID, findSingleSelectID, formatDateForProject, calculateDueDate }
