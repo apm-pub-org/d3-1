4089d296b03c3e24ffa64343f7a75e3f1fd58a82
Sarah Edwards <skedwards88@github.com>
2023-11-20 15:20:45 -0800

    Update docsOpenDocsIssue.js

--- automation/docsOpenDocsIssue.js
+++ automation/docsOpenDocsIssue.js
@@ -1,42 +1,46 @@
-import { setOutput, setFailed } from "@actions/core";
-import { getOctokit, context } from "@actions/github";
+import {setOutput, setFailed} from "@actions/core";
+import {getOctokit, context} from "@actions/github";
 
 async function docsOpenDocsIssue() {
-  console.log("Opening docs-content issue...")
+  console.log("Opening docs-content issue...");
 
   const token = process.env.DOCS_TOKEN;
   const octokit = getOctokit(token);
 
-  let labels = ['priority-0', 'new-release']
-  labels = labels.concat(JSON.parse(process.env.GHES_LABELS))
+  let labels = ["priority-0", "new-release"];
+  labels = labels.concat(JSON.parse(process.env.GHES_LABELS));
   switch (process.env.TIER) {
-    case 'Tier 1':
-      labels.push("Tier 1")
+    case "Tier 1":
+      labels.push("Tier 1");
       break;
-    case 'Tier 2':
-      labels.push("Tier 2")
+    case "Tier 2":
+      labels.push("Tier 2");
       break;
-    case 'Tier 3':
-      labels.push("Tier 3")
+    case "Tier 3":
+      labels.push("Tier 3");
       break;
-    case 'Tier 4':
-      labels.push("Tier 4")
+    case "Tier 4":
+      labels.push("Tier 4");
       break;
     default:
-      console.log("Tier not identified.")
+      console.log("Tier not identified.");
   }
+
+  console.log(`Labels to add: ${JSON.stringify(labels)}`);
 
   const docsContentIssue = await octokit.rest.issues.create({
     owner: context.repo.owner,
-    repo: "second-repo",//todo change to docs-content
+    repo: "second-repo",
     title: process.env.NEW_TITLE,
     body: process.env.NEW_BODY,
-    labels: labels
+    labels: labels,
   });
+
+  console.log(`Created docs-content issue ${docsContentIssue}`);
 
   setOutput("docsIssueNumber", docsContentIssue.data.number);
 }
 
 docsOpenDocsIssue().catch((error) => {
-  setFailed(`Action failed with error: ${error}`);
+  setFailed(`docsOpenDocsIssue failed with error: ${error}`);
 });
