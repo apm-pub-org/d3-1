2c5e2a8a2b8bd0a77e79e5d9ab27214dc95a2f49
Sarah Edwards <skedwards88@github.com>
2023-11-20 15:46:54 -0800

    Update docsParseReleaseIssue.js

--- automation/docsParseReleaseIssue.js
+++ automation/docsParseReleaseIssue.js
@@ -47,20 +47,33 @@ function compileIssueBody(shipDate, issue) {
 
   <!-- issue-fields end -->
 
-  ## How to use this issue
+  ## Required resources
+
+  Docs team [lead time](https://github.com/github/docs-team/blob/main/contributing-to-docs/how-to-work-with-docs-content.md#lead-times-for-documenting-releases) to prepare documentation is **2 weeks minimum**. This lead time begins _after_ the following is provided:
+
+  - [ ] Release FAQ or similar information
+  - [ ] Access to the feature through feature flag or staff ship
+  - [ ] _other information here_
+
+  <details>
+  <summary>How to use this issue</summary>
 
   Every issue in github/releases automatically opens an issue in github/docs-content for the Docs team to triage.
 
-  - Writer FRs: Add the product label(s) for the appropriate Docs focus team. If you need help, consult a [focus area DRI](https://github.com/github/docs-content/tree/main/focus-areas).
-  - Docs focus area DRIs: For public beta and GA releases, determine whether the release needs new documentation or requires updates to existing docs, collaborating with the product team as needed. If no docs updates are needed, close this issue as not planned. For alpha and private beta releases, work with the product team as needed to support self-publishing.
+  - **Writer FRs**: Add the product label(s) for the appropriate Docs focus team. If you need help, consult a [focus area DRI](https://github.com/github/docs-content/tree/main/focus-areas).
+  - **Docs focus area DRIs**: For public beta and GA releases, determine whether the release needs new documentation or requires updates to existing docs, collaborating with the product team as needed. If no docs updates are needed, close this issue as not planned. For alpha and private beta releases, work with the product team as needed to support self-publishing.
 
-  ## Documentation at different release phases
+  </details>
+
+  <details>
+  <summary>Documentation at different release phases</summary>
 
   Public documentation on GitHub Docs begins at public beta, or at private beta if the beta is widely publicized or made available to a broad user base.
 
   For alphas and private betas, product teams can create and publish their own private documentation with Docs team help. Documentation at these phases is optional but highly recommended. See the [Preview Guide](https://github.com/github/product/blob/main/GitHub%20Preview%20Resources/preview-guide.md#documenting-your-preview) for instructions.
 
   If this release has a small docs impact, invite the product manager or another stakeholder to update the docs after you create the content design plan.
+  </details>
   `;
 }
 
